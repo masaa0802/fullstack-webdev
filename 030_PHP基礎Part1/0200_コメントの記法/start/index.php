23e593d6cb52dcff0223d8c43be1fe054c10228b
masaa0802 <masaakigoto0802@gmail.com>
2022-02-18 15:53:53 +0900

    first commit

---
+++ 030_PHP基礎Part1/0200_コメントの記法/start/index.php
@@ -0,0 +1,8 @@
+<?php 
+/*
+echo 'hello'
+echo 'hello'
+echo 'hello'
+echo 'hello'
+*/
+?>
---
+++ 030_PHP基礎Part1/0300_変数定義/start/index.php
@@ -0,0 +1,9 @@
+<?php 
+$person_name = 'Tim';
+/* 命名規則メモ
+　person_name ←　スネークケース
+　personName ←　キャメル(らくだ)ケース
+*/
+echo 'hello,' . $person_name . '<br>';
+echo 'bye,' . $person_name;
+
