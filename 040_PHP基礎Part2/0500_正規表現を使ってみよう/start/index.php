074fedb1fd16bd51a802a53d4dec47842e70f210
masaa0802 <masaakigoto0802@gmail.com>
2022-02-24 00:12:44 +0900

    add

---
+++ 040_PHP基礎Part2/0500_正規表現を使ってみよう/start/index.php
@@ -0,0 +1,8 @@
+<?php 
+$char = '<h2>1ZAabd12_sscc</h2>';
+if(preg_match("/<h2>(.{0,})<\/h2>/i", $char, $result)){
+  echo'検索成功';
+  print_r($result);
+} else {
+  echo'検索失敗';
+}
--- 040_PHP基礎Part2/0600_理解度チェック（正規表現）/start/index.php
+++ 040_PHP基礎Part2/0600_理解度チェック（正規表現）/start/index.php
@@ -28,7 +28,13 @@
  * wd3-2132 -> NG
  * 124-56789 -> NG
  */
-
+$zipcode = '213-0244';
+if (preg_match("/^\d{3}-\d{4}$/",$zipcode, $result)){
+  echo '<div>郵便番号が正しいです。</div>';
+  // print_r($result);
+} else {
+  echo'<div>郵便番号が不正です。</div>';
+}
 
 /**
  * Email
@@ -39,9 +45,35 @@
  * example-0.00@ex.co.jp -> OK
  * example/0.00@ex.co.jp -> NG
  */
-
+$email = 'example000@gmail.com';
+if (preg_match("/^[\w.\-]+@+[\w.\-]+\.[\w\.\-]+$/",$email, $result)){
+  echo '<div>メールアドレスは正しいです。</div>';
+  // print_r($result);
+} else {
+  echo'<div>メールアドレスが間違ってます。</div>';
+}
 
 /**
  * HTML
  * 見出しタグ(h1~h6)の中身のみ取得してみよう。
  */
+
+$html = 
+'<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>見出し1</h1>
+  <h2>見出し2</h2>
+  <h3>見出し3</h3>
+  </body>
+</html>';
+
+if (preg_match_all("/<h[1-6]>(.+)<\/h[1-6]>/",$html, $result)){
+  print_r($result[count($result) - 1]);
+} 
